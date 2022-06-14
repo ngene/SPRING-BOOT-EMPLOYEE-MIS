a5ec1607ec7a11cfd4aac9ff359cc1b0f4208c34
ngene <ngenedad@gmail.com>
2022-06-14 07:29:13 -0700

    Add files via upload

---
+++ employee-mis-ui/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return 
+    <div className="">
+
+      <div className="h-16 flex items-center">
+<p className="text-white font-bold"> Employee MIS</p>
+
+      </div>
+      
+    </div>
+  
+}
+
+export default App;
